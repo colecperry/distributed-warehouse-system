b7d25481d8042cf4a942410ef925e721bdcbfb28
Cole Perry <colecperry@gmail.com>
2025-11-28 01:10:46 +0800

    Update credit card service with simulated delays

--- microservices/credit-card-service/src/test/java/com/example/creditcardauthorizer/CreditCardAuthorizerApplicationTests.java
+++ microservices/credit-card-service/src/test/java/com/example/creditcardauthorizer/CreditCardAuthorizerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.creditcardauthorizer;
+package com.cs6650.creditcardauthorizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CreditCardAuthorizerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+  }
 }
